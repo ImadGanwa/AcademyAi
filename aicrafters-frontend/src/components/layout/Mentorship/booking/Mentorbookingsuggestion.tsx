03bfce478b0c425fd3fbff8dfa95f1881390a8a0
ayman.amkassou <ayman.amkassou@gmail.com>
2025-05-19 18:38:04 +0100

    fixed internalisation for a hardcoded page

--- aicrafters-frontend/src/components/layout/Mentorship/booking/Mentorbookingsuggestion.tsx
+++ aicrafters-frontend/src/components/layout/Mentorship/booking/Mentorbookingsuggestion.tsx
@@ -3,6 +3,7 @@ import { Box, Typography } from '@mui/material';
 import styled from 'styled-components';
 import { Mentor } from '../card/MentorCard';
 import { AccessTime as ClockIcon, Videocam as VideoIcon } from '@mui/icons-material';
+import { useTranslation } from 'react-i18next';
 
 const CardContainer = styled(Box)`
   display: flex;
@@ -60,27 +61,30 @@ interface MentorInfoCardProps {
 }
 
 export const MentorInfoCard: React.FC<MentorInfoCardProps> = ({ mentor }) => {
+  const { t } = useTranslation();
+  
   return (
     <CardContainer>
       <BookingSection>
-        <BookingTitle>Book a Session with {mentor.fullName}</BookingTitle>
+        <BookingTitle>{t('mentorship.booking.bookSession', 'Book a Session with')} {mentor.fullName}</BookingTitle>
         <BookingDescription>
-          Choose a date and time that works for you. Once confirmed, your mentor will receive the request and you'll both get a calendar invite.
+          {t('mentorship.booking.description', 'Choose a date and time that works for you. Once confirmed, your mentor will receive the request and you\'ll both get a calendar invite.') as string}
         </BookingDescription>
+        // TODO: add internalisation for Mentor Dashboard
 
         <SessionDetails>
           <DetailItem>
             <DetailIcon>
               <ClockIcon />
             </DetailIcon>
-            <DetailText>30 min</DetailText>
+            <DetailText>{t('mentorship.booking.duration', '30 min') as string}</DetailText>
           </DetailItem>
 
           <DetailItem>
             <DetailIcon>
               <VideoIcon />
             </DetailIcon>
-            <DetailText>Zoom Conference</DetailText>
+            <DetailText>{t('mentorship.booking.platform', 'Zoom Conference') as string}</DetailText>
           </DetailItem>
         </SessionDetails>
       </BookingSection>
