099b431fd48828dd64232a50b2402cd9557ecf70
OthmanMoussaoui <moussaouiothman1@gmail.com>
2025-06-05 00:51:05 +0100

    Remove dist directory from tracking and add to .gitignore

--- aicrafters-backend/dist/utils/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createError = void 0;
-const createError = (message, error) => {
-    return Object.assign({ message }, (error && { error: error.message || error }));
-};
-exports.createError = createError;
